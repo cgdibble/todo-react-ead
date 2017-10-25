54420806f4ce386dae00f3d8e9f435315a3c7f66
Charlie Dibble <cgdibble@gmail.com>
2017-10-25 08:59:01 -0700

    used created react app, added regenerator runtime as per effects-as-data-redux instructions

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'regenerator-runtime/runtime'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
